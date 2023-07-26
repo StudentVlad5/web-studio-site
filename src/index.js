861dba16c4eaebabf47f95e05e5f442e78a095eb
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-07-26 19:40:08 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from 'components/App';
-import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
-import { ThemeStatus } from 'components/ThemeStatus/ThemeProvider';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "components/App";
+import { GlobalStyle } from "components/baseStyles/GlobalStyle";
+import { ThemeStatus } from "components/ThemeStatus/ThemeProvider";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="brand-maze">
+    <BrowserRouter basename="web-studio-site">
       <I18nextProvider i18n={i18n}>
         <ThemeStatus>
           <GlobalStyle />
