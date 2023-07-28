d545964a394cc8fb417387e6a4b6eed708639248
JuliaGolban <julia.j.shcherban@gmail.com>
2023-07-28 15:48:55 +0300

    add sidebar and fix header

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,2 +1,10 @@
-export const githubRef = document.querySelector(".icon-github");
-export const linkedinRef = document.querySelector(".icon-linkedin");
+export default function getRefs() {
+  return {
+    //========== mobile menu ==========
+    menuBtnRef: document.querySelector('.js-mobile-menu-button'),
+    mobileMenuRef: document.querySelector('.js-mobile-menu'),
+    menuLinks: document.querySelector('.js-mobile-menu-links'),
+
+    //==========  ==========
+  };
+}
