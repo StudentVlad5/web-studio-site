2fbfbbd268feff6ec32e598545d16bbab66b4889
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-07-31 13:51:59 +0300

    desktop cases

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./js/refs/refs";
 import "./js/localStorage/lsService";
 import "./js/components/to-top-button";
 import "./js/components/mobile-menu";
+import "./js/components/slider";
 import "./js/functions/audio-player_1";
 import "./js/functions/audio-player_2";
 import "./js/functions/audio-player_3";
