ce9718f7100a647a0feb5ba79ec880755a8bc2c9
JuliaGolban <julia.j.shcherban@gmail.com>
2023-08-01 17:21:26 +0300

    add about styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/refs/refs';
 import './js/localStorage/lsService';
 import './js/components/header-on-scroll';
 import './js/components/mobile-menu';
+import './js/components/list-on-scroll';
 import './js/components/to-top-button';
 import './js/functions/audio-player_1';
 import './js/functions/audio-player_2';
