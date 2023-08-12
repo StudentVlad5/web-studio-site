1c8e30a2f2017b07f74c9b3855c3c0ebf993e937
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-08-12 16:35:52 +0300

    fix styles testimonials

--- src/js/components/testimonialsOfClients.js
+++ src/js/components/testimonialsOfClients.js
@@ -110,112 +110,112 @@ function addClientsRewiers() {
         data-aos-anchor=${item.anchor}
         data-aos-duration=${item.duration}
       >
-      <div class="testimonials__client-coordinate${item.clientId}" style="position:relative">
-        <div class="testimonials__client-box">
-          <div class="testimonials__client-wrap">
-            <img
-              class="testimonials__client-img"
-              src=${item.clientImg}
-              alt="client number ${item.clientId}"
-            />
-            <div class="testimonials__client-data">
-              <p>${item.clientName}</p>
-              <p>${item.clientCompany}</p>
+        <div class="testimonials__client-coordinate${item.clientId}" style="position:relative">
+          <div class="testimonials__client-box">
+            <div class="testimonials__client-wrap">
+              <img
+                class="testimonials__client-img"
+                src=${item.clientImg}
+                alt="client number ${item.clientId}"
+              />
+              <div class="testimonials__client-data">
+                <p>${item.clientName}</p>
+                <p>${item.clientCompany}</p>
+              </div>
             </div>
-          </div>
-          <div class="wrapper">
-            <div
-              class="music-container player"
-              id="music-container${item.clientId}"
-            >
-              <div class="testimonials-navigation player__road">
-                <div class="player__row">
-                  <button
-                    type="button"
-                    id="play${item.clientId}"
-                    data-play="play"
-                    data-audio="${item.clientId}"
-                    class="action-btn action-btn-big player__btn play--btn"
-                  >
-                  <svg
-                  class="player__icon fa-pause"
-                  width="12"
-                  height="12"
-                >
-                <use href="${sprite}#play"></use>
-                  </button>
-                  <button
-                    type="button"
-                    id="pause${item.clientId}"
-                    data-play="pause"
-                    data-audio="${item.clientId}"
-                    class="action-btn action-btn-big player__btn hidden pause--btn"
-                  >
-                    <svg
-                      class="player__icon fa-pause"
-                      width="12"
-                      height="12"
+            <div class="player__wrapper">
+              <div
+                class="music-container player"
+                id="music-container${item.clientId}"
+              >
+                <div class="testimonials-navigation player__road">
+                  <div class="player__row">
+                    <button
+                      type="button"
+                      id="play${item.clientId}"
+                      data-play="play"
+                      data-audio="${item.clientId}"
+                      class="action-btn action-btn-big player__btn play--btn"
                     >
-                      <use href="${sprite}#pause"></use>
-                    </svg>
-                  </button>
-                  <p class="player__name">${item.clientTitle}</p>
-                </div>
-                <div class="player__row testimonials_row">
-                <div class="sound-set" data-audio=${item.clientId}>
-                    <svg class="player__icon" width="15" height="15" data-sound="off" data-audio=${item.clientId}>
-                        <use href="${sprite}#sound_off"></use>
-                    </svg>
-                    <div class="progress hidden testimonials" data-sound="on" data-audio=${item.clientId}>
-                        <div class="progress__bar"></div>
-                        <div class="progress__bar"></div>
-                        <div class="progress__bar"></div>
-                        <div class="progress__bar"></div>
+                    <svg
+                    class="player__icon fa-pause"
+                    width="12"
+                    height="12"
+                  >
+                  <use href="${sprite}#play"></use>
+                    </button>
+                    <button
+                      type="button"
+                      id="pause${item.clientId}"
+                      data-play="pause"
+                      data-audio="${item.clientId}"
+                      class="action-btn action-btn-big player__btn hidden pause--btn"
+                    >
+                      <svg
+                        class="player__icon fa-pause"
+                        width="12"
+                        height="12"
+                      >
+                        <use href="${sprite}#pause"></use>
+                      </svg>
+                    </button>
+                    <p class="player__name">${item.clientTitle}</p>
+                  </div>
+                  <div class="player__row testimonials_row">
+                  <div class="sound-set" data-audio=${item.clientId}>
+                      <svg class="player__icon" width="15" height="15" data-sound="off" data-audio=${item.clientId}>
+                          <use href="${sprite}#sound_off"></use>
+                      </svg>
+                      <div class="progress hidden testimonials" data-sound="on" data-audio=${item.clientId}>
+                          <div class="progress__bar"></div>
+                          <div class="progress__bar"></div>
+                          <div class="progress__bar"></div>
+                          <div class="progress__bar"></div>
+                      </div>
+                  </div>
+              </div>
+                    <div class="time">
+                      <span class="time-elapsed_${item.clientId}" 
+                      data-play="time" 
+                      data-audio="${item.clientId}">
+                        ${item.time}
+                      </span>
                     </div>
+                  </div>
                 </div>
-            </div>
-                  <div class="time">
-                    <span class="time-elapsed_${item.clientId}" 
-                    data-play="time" 
-                    data-audio="${item.clientId}">
-                      ${item.time}
-                    </span>
+                <div class="testimonials__subtitlesBtn">
+                  <button
+                    type="button"
+                    title="swithSubtitle"
+                    class="player__switch-subtitle testimonials__switchSubtitleBtn switchSubtitleBtn_${item.clientId}"
+                    data-count="switchSubtitleBtn_${item.clientId}"
+                    data-audio=${item.clientId}
+                    >
+                    <svg class="player__icon" width="10" height="10">
+                      <use href="${sprite}#arrow-right2"></use>
+                    </svg>
+                    A
+                  </button>
+                </div>
+                <div class="testimonials__subtitle-container">
+                  <div
+                    class="player__subtitle hidden testimonials-subtitles testimonials-subtitles_${item.clientId}"
+                    data-audio=${item.clientId}
+                    >
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Dignissimos eaque ut autem ad unde. Repellat delectus, sed quo,
+                      accusantium ad recusandae inventore illum fuga est, provident
+                      obcaecati molestiae fugit et.
+                    </p>
+                    <svg class="player__icon testimonials-subtitles--icon" width="30" height="15">
+                    <use href="${sprite}#triangle-left"></use>
+                    </svg>
                   </div>
                 </div>
               </div>
-              <div class="testimonials__subtitlesBtn">
-                <button
-                  type="button"
-                  title="swithSubtitle"
-                  class="player__switch-subtitle testimonials__switchSubtitleBtn switchSubtitleBtn_${item.clientId}"
-                  data-count="switchSubtitleBtn_${item.clientId}"
-                  data-audio=${item.clientId}
-                  >
-                  <svg class="player__icon" width="10" height="10">
-                    <use href="${sprite}#arrow-right2"></use>
-                  </svg>
-                  A
-                </button>
-              </div>
             </div>
           </div>
-        </div>
-        <div class="testimonials__subtitle-container">
-          <div
-            class="player__subtitle hidden testimonials-subtitles testimonials-subtitles_${item.clientId}"
-            data-audio=${item.clientId}
-            >
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Dignissimos eaque ut autem ad unde. Repellat delectus, sed quo,
-              accusantium ad recusandae inventore illum fuga est, provident
-              obcaecati molestiae fugit et.
-            </p>
-            <svg class="player__icon testimonials-subtitles--icon" width="30" height="15">
-            <use href="${sprite}#triangle-left"></use>
-            </svg>
-          </div>
-        </div>
         </div>
       </li>
   `;
